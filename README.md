4dfca35efb94bcc5a53e5630bdbac43800cf4151
Víctor Chagas <victorchagaslm@gmail.com>
2023-12-02 15:27:33 -0300

    refactor: Movendo todo o projeto para a nova arquiterura

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # InvisiMark
+
+# Comando para executar o Tailwind CSS
+
+```
+npx tailwindcss -i ./invisimark/static/src/input.css -o ./invisimark/static/css/main.css --watch
+```
--- invisimark/blueprints/views.py
+++ invisimark/blueprints/views.py
@@ -1,7 +1,52 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+users = [
+    {"email": "user1@example.com", "password": "password1"},
+    {"email": "user2@example.com", "password": "password2"},
+]
 
 
 def init_app(app):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+
+            for user in users:
+                if user['email'] == email and user['password'] == password:
+                    return redirect(url_for('dashboard'))
+
+            return "Credenciais inválidas"
+
+        return render_template('auth/login.html')
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+
+            for user in users:
+                if user['email'] == email and user['password'] == password:
+                    return redirect(url_for('/'))
+
+            return "Credenciais inválidas"
+
+        return render_template('auth/register.html')
+
+    @app.route('/dashboard')
+    def dashboard():
+        return render_template('dashboard/index.html')
+
+    @app.route('/dashboard/insertion')
+    def insertion():
+        return render_template('dashboard/insertion.html')
+
+    @app.route('/dashboard/myprofile')
+    def myprofile():
+        return render_template('dashboard/myprofile.html')
